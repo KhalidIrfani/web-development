70d7d6131d672ab818099e8adfa68fb1fe7a2674
KhalidIrfani <107645929+KhalidIrfani@users.noreply.github.com>
2022-06-26 15:47:33 +0500

    update

---
+++ health/img/script.js
@@ -0,0 +1,29 @@
+ let Assalamoalikum = document.querySelector('button');
+ Assalamoalikum.addEventListener('click', inputMsg);
+
+ function inputMsg() {
+     let name = prompt(' Enter name of studemt');
+     Assalamoalikumbutton.textcontent = 'roll no.1:' + name;
+ }
+
+ // // for (let i = 5; i < 50; i = i + 5) {
+ // //     if (i % 2 == 0)
+ // //         console.log(i)
+ // // }
+ // // let j = 10;
+ // // do {
+ // //     console.log("Assalam - o - Alikum")
+ // //     j--
+ // // }
+ // // while (j > 0)
+
+ // function COOKMEGI(MEGI, pani, tapeeli) {
+ //     console.log("your megi readi in " +
+ //         MEGI * 2 +
+ //         " min " +
+ //         " and ingredients used are:- " +
+ //         MEGI + " megi " +
+ //         pani + " cups of water " +
+ //         " using " + tapeeli + " pan ")
+ // }
+ // COOKMEGI(4, 8, 1);
